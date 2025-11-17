23be02e02438230c60e3843ff976337e6262c72a
Dayana Ferrufino <dayanaferrufino3@gmail.com>
2025-11-17 15:27:17 -0500

    CG3-124 : Fix upload multiple document tests
    - previous test was looking for file name ending in .txt, fixtures uploaded were not .txt, it would fail

--- Playwright/tests/Document-Functions/test_multiple_uploads.py
+++ Playwright/tests/Document-Functions/test_multiple_uploads.py
@@ -1,4 +1,4 @@
-# tests/test_upload_multiple_files.py
+# test_upload_multiple_files.py
 from datetime import datetime
 from pathlib import Path
 from playwright.sync_api import expect
@@ -11,24 +11,22 @@ def _make_file(fixtures_dir: Path, stem: str, ext=".txt") -> Path:
     return fpath
 
 def _upload_file(page, fpath: Path):
-    # Re-locates the file input everytime a file uploads in case the DOM re-renders
     file_input = page.locator('input[type="file"]').first
-    expect(file_input).to_be_visible(timeout=10000)
+    expect(file_input).to_be_visible(timeout=10_000)
 
-    # upload
     file_input.set_input_files(str(fpath))
 
-    # verify the upload completed 
-    # checks if the filename is somwewhere on the page
+    visible_name = fpath.stem
+
     try:
-        expect(page.get_by_text(fpath.name, exact=False)).to_be_visible(timeout=900)
+        expect(page.get_by_text(visible_name, exact=False)).to_be_visible(timeout=10_000)
         return
     except Exception:
         pass
 
-    # Fallbacks checks if file upload failed
+    # check that the "no documents" empty state is gone
     try:
-        expect(page.get_by_text("No documents uploaded yet", exact=False)).not_to_be_visible(timeout=900)
+        expect(page.get_by_text("No documents uploaded yet", exact=False)).not_to_be_visible(timeout=3_000)
         return
     except Exception:
         pass
@@ -36,7 +34,7 @@ def _upload_file(page, fpath: Path):
     # Debug on failure
     page.screenshot(path=f"upload_debug_{fpath.stem}.png", full_page=True)
     Path(f"upload_debug_{fpath.stem}.html").write_text(page.content(), encoding="utf-8")
-    raise AssertionError(f"Upload did not appear for: {fpath.name}")
+    raise AssertionError(f"Upload did not appear for: {visible_name}")
 
 def test_upload_three_files(page_with_auth):
     page = page_with_auth #starts on /upload page (no need to login)
